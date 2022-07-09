db2d0b56e46e72180a8f38c074082a8e94220c2a
Andrew Monteiro <andrew.agoravai@gmail.com>
2022-07-09 00:02:33 -0300

    feat: read and write player's guess

--- game.c
+++ game.c
@@ -5,5 +5,12 @@ int main() {
 	printf("*******************************\n");
 	printf("* Welcome to my Guessing Game *\n");
 	printf("*******************************\n");
+	
+	int guess;
+	
+	printf("What's your guess?\n");
+	scanf("%d", &guess);
+	
+	printf("Your guess was: %d\n", guess);
 }
 
