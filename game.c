9861239448ff99802931b86008dcb50f2f0ebd2a
Andrew Monteiro <andrew.agoravai@gmail.com>
2022-07-09 00:07:21 -0300

    feat: display different messages if the player guesses or not the secret number

--- game.c
+++ game.c
@@ -6,11 +6,17 @@ int main() {
 	printf("* Welcome to my Guessing Game *\n");
 	printf("*******************************\n");
 	
+	int secretNumber = 42;
 	int guess;
 	
 	printf("What's your guess?\n");
 	scanf("%d", &guess);
 	
-	printf("Your guess was: %d\n", guess);
+	if (guess == secretNumber) {
+		printf("Congratulations! You're right!\n");
+	} else {
+		printf("You missed! =/\n");
+		printf("But that's alright!! You can try again!\n");
+	}
 }
 
